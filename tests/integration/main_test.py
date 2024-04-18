bcff1ccb3b5dabb877154c13b8d9b9b337842622
Juan Biancuzzo <jbiancuzzo@fi.uba.ar>
2024-04-18 11:42:24 -0400

    Intentando avanzar con los test de integracion

--- tests/integration/main_test.py
+++ tests/integration/main_test.py
@@ -1,19 +1,36 @@
 import unittest
 
 from mininet.net import Mininet
+from mininet.node import OVSSwitch, Controller, RemoteController
 from mininet.topolib import TreeTopo
+from mininet.log import setLogLevel
+from mininet.cli import CLI
+
+class MultiSwitch( OVSSwitch ):
+    "Custom Switch() subclass that connects to different controllers"
+    def start( self, controllers ):
+        return OVSSwitch.start( self, [ cmap[ self.name ] ] )
 
 class TestMininet(unittest.TestCase):
 
     def test01(self):
-        tree4 = TreeTopo(depth=2,fanout=2)
+        setLogLevel( 'info' )
 
-        net = Mininet(topo=tree4)
+        # Two local and one "external" controller (which is actually c0)
+        # Ignore the warning message that the remote isn't (yet) running
+        c0 = Controller( 'c0', port=6633 )
+        c1 = Controller( 'c1', port=6634 )
+        c2 = RemoteController( 'c2', ip='127.0.0.1', port=6633 )
+        
+        cmap = { 's1': c0, 's2': c1, 's3': c2 }
+
+        topo = TreeTopo( depth=2, fanout=2 )
+        net = Mininet( topo=topo, switch=MultiSwitch, build=False, waitConnected=True )
+        for c in [ c0, c1 ]:
+            net.addController(c)
+        net.build()
         net.start()
-
-        h1, h4  = net.hosts[0], net.hosts[3]
-        print(h1.cmd('ping -c1 %s' % h4.IP()))
-
+        CLI( net )
         net.stop()
 
 if __name__ == '__main__':
