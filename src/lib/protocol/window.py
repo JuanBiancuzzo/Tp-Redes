6fd93b601653c396daa76f6e3d0b8298ebe1e009
Rafael Berenguel <rafaelberenguel15@gmail.com>
2024-04-29 17:28:00 -0300

    Se actualiza window para que use un deque

--- src/lib/protocol/window.py
+++ src/lib/protocol/window.py
@@ -1,7 +1,9 @@
+from collections import deque
+
 class Window:
     def __init__(self, size: int):
         self.size = size
-        self.window = []
+        self.window = deque()
         
     def __len__(self):
         return len(self.window)
@@ -14,7 +16,11 @@ class Window:
     
     # Si el tamaño no cambia me mandaron un ack de uno que ya me ackearon, por lo que se perdió el siguiente.
     def remove_acked_segments(self, ack_number):
-        self.window = [segment for segment in self.window if segment.sequence_number >= ack_number]
-    
+        while len(self.window) > 0 and self.window[0].header.seq_num < ack_number:
+            self.window.popleft()
+
     def is_full(self):
         return len(self.window) == self.size
+
+    def get_oldest_segment(self):
+        return self.window[0] if len(self.window) > 0 else None
