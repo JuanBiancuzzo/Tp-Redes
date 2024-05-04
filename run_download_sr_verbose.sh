561e0d67ad73f0abb1f90ca964a8e892a2fe7b11
Juan Biancuzzo <jbiancuzzo@fi.uba.ar>
2024-05-04 11:27:27 -0300

    Modificando el comando de bash para no tener que modificar el codigo

--- run_download_sr_verbose.sh
+++ run_download_sr_verbose.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+dir_actual=$(pwd)
+download_dir="files/download_files"
+
+cd $download_dir
+
 # Run the Python program using python3 with relative path
-python3 src/download.py -v -H 127.0.0.1 -p 3006 -d files/download_files -n $1 -r
+python3 $dir_actual/src/download.py -v -H 127.0.0.1 -p 3006 -n $1 -r
+
+cd dir_actual
--- run_download_sw_verbose.sh
+++ run_download_sw_verbose.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+dir_actual=$(pwd)
+download_dir="files/download_files"
+
+cd $download_dir
+
 # Run the Python program using python3 with relative path
-python3 src/download.py -v -H 127.0.0.1 -p 3006 -d files/download_files -n $1 -w
+python3 $dir_actual/src/download.py -v -H 127.0.0.1 -p 3006 -n $1 -w
+
+cd $dir_actual
